3e54d9601629f8692a5ba159821821b8432232d2
Michael König <michael.koenig@blue-yonder.com>
2017-12-11 17:42:26 +0100

    Refactor: extract quantity checks to helper function

--- shipping/src/shipping.cpp
+++ shipping/src/shipping.cpp
@@ -2,21 +2,30 @@
 
 #include <stdexcept>
 
+namespace {
+
+    void check_physical_attributes(double length, double width, double height, double weight)
+    {
+        if (length <= 0) {
+            throw std::runtime_error("The parcel must have positive length");
+        }
+        if (width <= 0) {
+            throw std::runtime_error("The parcel must have positive width");
+        }
+        if (height <= 0) {
+            throw std::runtime_error("The parcel must have positive height");
+        }
+        if (weight <= 0) {
+            throw std::runtime_error("The parcel must have positive weight");
+        }
+    }
+
+}
+
 namespace shipping {
 
 double get_reindeer_price(double length, double width, double height, double weight) {
-    if (length <= 0) {
-        throw std::runtime_error("The parcel must have positive length");
-    }
-    if (width <= 0) {
-        throw std::runtime_error("The parcel must have positive width");
-    }
-    if (height <= 0) {
-        throw std::runtime_error("The parcel must have positive height");
-    }
-    if (weight <= 0) {
-        throw std::runtime_error("The parcel must have positive weight");
-    }
+    check_physical_attributes(length, width, height, weight);
 }
 
 }
