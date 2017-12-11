4cd1ef51972effda5e08af7f702586d39ee678a1
Michael König <michael.koenig@blue-yonder.com>
2017-12-11 17:49:50 +0100

    Refactor: Remove superfluous dummy test

--- tests/test_shipping.cpp
+++ tests/test_shipping.cpp
@@ -6,10 +6,6 @@
 
 using shipping::get_reindeer_price;
 
-TEST(GetReindeerPriceTest, Dummy) {
-    get_reindeer_price(1, 2, 3, 4);
-}
-
 TEST(GetReindeerPriceTest, FailOnNonPositiveLength) {
     EXPECT_THROW(get_reindeer_price(-1, 2, 3, 4), std::runtime_error);
     EXPECT_THROW(get_reindeer_price(0, 2, 3, 4), std::runtime_error);
