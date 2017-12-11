9911fa8a65a2c24b5e8b2e61acb4ee4ff1289d15
Michael König <michael.koenig@blue-yonder.com>
2017-12-11 17:37:03 +0100

    Success

--- shipping/src/shipping.cpp
+++ shipping/src/shipping.cpp
@@ -8,8 +8,8 @@ double get_reindeer_price(double length, double width, double, double) {
     if (length <= 0) {
         throw std::runtime_error("The parcel must have positive length");
     }
-    if (width < 0) {
-        throw std::runtime_error("The parcel must not have negative width");
+    if (width <= 0) {
+        throw std::runtime_error("The parcel must have positive width");
     }
 }
 
