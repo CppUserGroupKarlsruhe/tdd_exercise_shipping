49c3e1aebe4e453a57ccbb40682dea6d33d68d71
Michael König <michael.koenig@blue-yonder.com>
2017-12-11 17:57:25 +0100

    Refactoring idea: treat negative dimensions differently from too large parcel (fails)

--- tests/test_shipping.cpp
+++ tests/test_shipping.cpp
@@ -5,6 +5,7 @@
 #include <stdexcept>
 
 using shipping::get_reindeer_price;
+using shipping::parcel;
 
 TEST(GetReindeerPriceTest, FailOnNonPositiveLength) {
     EXPECT_THROW(get_reindeer_price(-1, 2, 3, 4), std::runtime_error);
@@ -24,6 +25,28 @@ TEST(GetReindeerPriceTest, FailOnNonNegativeHeight) {
 TEST(GetReindeerPriceTest, FailOnNonNegativeWeight) {
     EXPECT_THROW(get_reindeer_price(1, 2, 3, -4), std::runtime_error);
     EXPECT_THROW(get_reindeer_price(1, 2, 4, 0), std::runtime_error);
+}
+
+// REFACTORING AREA --->
+
+TEST(ParcelTest, FailOnNonPositiveLength) {
+    EXPECT_THROW(parcel(-1, 2, 3, 4), std::runtime_error);
+    EXPECT_THROW(parcel(0, 2, 3, 4), std::runtime_error);
+}
+
+TEST(ParcelTest, FailOnNonNegativeWidth) {
+    EXPECT_THROW(parcel(1, -2, 3, 4), std::runtime_error);
+    EXPECT_THROW(parcel(1, 0, 3, 4), std::runtime_error);
+}
+
+TEST(ParcelTest, FailOnNonNegativeHeight) {
+    EXPECT_THROW(parcel(1, 2, -3, 4), std::runtime_error);
+    EXPECT_THROW(parcel(1, 2, 0, 4), std::runtime_error);
+}
+
+TEST(ParcelTest, FailOnNonNegativeWeight) {
+    EXPECT_THROW(parcel(1, 2, 3, -4), std::runtime_error);
+    EXPECT_THROW(parcel(1, 2, 4, 0), std::runtime_error);
 }
 
 TEST(GetReindeerPriceTest, FailOnParcelTooLarge) {
