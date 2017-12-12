8d54a22245914590595b47a52ca15dd8bae3bd94
Michael König <michael.koenig@blue-yonder.com>
2017-12-12 09:51:51 +0100

    Success

--- shipping/src/shipping.cpp
+++ shipping/src/shipping.cpp
@@ -39,7 +39,7 @@ double get_reindeer_price(parcel const & parcel)
     if (parcel.width > 30) {
         throw std::runtime_error("The parcel is too large");
     }
-    if ((parcel.length > 30) or (parcel.width > 20) or (parcel.height > 20)) {
+    if ((parcel.length > 30) or (parcel.width > 20) or (parcel.height > 20) or (parcel.weight > 2)) {
         return 4.99;
     }
 
