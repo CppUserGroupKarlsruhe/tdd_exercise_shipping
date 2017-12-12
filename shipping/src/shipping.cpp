3ce1fd8fbd79ddf1ef8001db38ccd6c16381ea41
Michael König <michael.koenig@blue-yonder.com>
2017-12-12 16:49:45 +0100

    Refactoring: Use pricelist in get_reindeer_price

--- shipping/src/shipping.cpp
+++ shipping/src/shipping.cpp
@@ -4,19 +4,17 @@
 
 namespace shipping {
 
+pricelist const reindeer_prices({{{30, 20, 20, 2}, 2.99},
+                                 {{40, 30, 20, 5}, 4.99},
+                                 {{60, 10, 60, 4}, 5.99},
+                                 {{120, 60, 60, 30}, 14.99}});
+
 double get_reindeer_price(parcel const & p)
 {
-    if (p.fits_into(parcel{30, 20, 20, 2})) {
-        return 2.99;
-    }
-    if (p.fits_into(parcel{40, 30, 20, 5})) {
-        return 4.99;
-    }
-    if (p.fits_into(parcel{60, 10, 60, 4})) {
-        return 5.99;
-    }
-    if (p.fits_into(parcel{120, 60, 60, 30})) {
-        return 14.99;
+    for (auto const & tariff : reindeer_prices.tariffs) {
+        if (tariff.available_for(p)) {
+            return tariff.get_price();
+        }
     }
     throw std::runtime_error("The parcel is too large");
 }
