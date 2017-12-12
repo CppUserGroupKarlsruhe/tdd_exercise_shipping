4b58b24bd1d0c70ece9671b0ead2e88859aa0ba7
Michael König <michael.koenig@blue-yonder.com>
2017-12-12 09:51:34 +0100

    Failure: Weight not yet considered

--- tests/test_shipping.cpp
+++ tests/test_shipping.cpp
@@ -49,4 +49,5 @@ TEST(GetReindeerPriceTest, SecondLowestPrice) {
     EXPECT_EQ(get_reindeer_price({30 + delta, 20, 20, 2}), 4.99);
     EXPECT_EQ(get_reindeer_price({30, 20 + delta, 20, 2}), 4.99);
     EXPECT_EQ(get_reindeer_price({30, 20, 20 + delta, 2}), 4.99);
+    EXPECT_EQ(get_reindeer_price({30, 20, 20, 2 + delta}), 4.99);
 }
