3ac25175ffd28cee736d15f1b16fb4a8cdd8951d
Michael König <michael.koenig@blue-yonder.com>
2017-12-12 10:58:22 +0100

    Refactor: Use fits_into() in get_reindeer_price()

--- shipping/src/shipping.cpp
+++ shipping/src/shipping.cpp
@@ -49,19 +49,15 @@ bool parcel::fits_into(parcel const & other) const
 }
 
 
-double get_reindeer_price(parcel const & parcel)
+double get_reindeer_price(parcel const & p)
 {
-    if (parcel.dimensions[0] > 41) {
-        throw std::runtime_error("The parcel is too large");
+    if (p.fits_into(parcel{30, 20, 20, 2})) {
+        return 2.99;
     }
-    if ((parcel.dimensions[0] > 40) or (parcel.dimensions[1] > 30) or (parcel.dimensions[2] > 20) or (parcel.weight > 4)) {
-        return 5.99;
-    }
-    if ((parcel.dimensions[0] > 30) or (parcel.dimensions[1] > 20) or (parcel.dimensions[2] > 20) or (parcel.weight > 2)) {
+    if (p.fits_into(parcel{40, 30, 20, 4})) {
         return 4.99;
     }
-
-    return 2.99;
+    throw std::runtime_error("The parcel is too large");
 }
 
 }
