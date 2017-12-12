35b251e8ecd54ea2dc4c5f91908e3857cba79a46
Michael König <michael.koenig@blue-yonder.com>
2017-12-12 10:53:40 +0100

    Failure: largest dimension ignored

--- tests/test_shipping.cpp
+++ tests/test_shipping.cpp
@@ -45,6 +45,7 @@ TEST(ParcelTest, FitsInto) {
     EXPECT_TRUE(p.fits_into(parcel{1, 2, 3, 4 + delta}));
     EXPECT_FALSE(p.fits_into(parcel{1 - delta, 2, 3, 4}));
     EXPECT_FALSE(p.fits_into(parcel{1, 2 - delta, 3, 4}));
+    EXPECT_FALSE(p.fits_into(parcel{1, 2, 3 - delta, 4}));
 }
 
 
