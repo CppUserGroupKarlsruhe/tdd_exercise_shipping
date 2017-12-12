5f425b8c618b0172f760070b6241932c9b5aca3b
Michael König <michael.koenig@blue-yonder.com>
2017-12-12 14:50:07 +0100

    Refactor (while failing): hide members of parcel struct to make them non-const

--- shipping/shipping/data_structures.h
+++ shipping/shipping/data_structures.h
@@ -5,11 +5,13 @@
 
 namespace shipping {
 
-struct parcel {
+class parcel {
+public:
     parcel(double length, double width, double height, double weight);
     bool fits_into(parcel const & other) const;
-    std::array<double, 3> const dimensions;
-    double const weight;
+private:
+    std::array<double, 3> dimensions;
+    double weight;
 };
 
 struct tariff {
--- tests/test_parcel.cpp
+++ tests/test_parcel.cpp
@@ -28,14 +28,6 @@ TEST(ParcelTest, FailOnNonNegativeWeight) {
     EXPECT_THROW(parcel(1, 2, 4, 0), std::runtime_error);
 }
 
-TEST(ParcelTest, StoresPhysicalDataWithSortedDimensions) {
-    parcel const p(2, 1, 3, 4);
-    EXPECT_EQ(p.dimensions[0], 3);
-    EXPECT_EQ(p.dimensions[1], 2);
-    EXPECT_EQ(p.dimensions[2], 1);
-    EXPECT_EQ(p.weight, 4);
-}
-
 TEST(ParcelTest, FitsInto) {
     parcel const p(1, 2, 3, 4);
     double const delta = 0.01;
