b6f5bd64933089fd8d6d0d1f2f20643c3b6d70cb
Michael König <michael.koenig@blue-yonder.com>
2017-12-13 12:53:11 +0100

    Refactoring: Reduce tests for reindeer prices to two tests

--- tests/test_shipping.cpp
+++ tests/test_shipping.cpp
@@ -25,57 +25,48 @@ TEST(GetBestPrice, PickCheapestMatchingTariff) {
     EXPECT_EQ(get_best_price(p, shipping::pricelist({more_expensive, cheapest})), cheapest.get_price());
 }
 
-TEST(GetReindeerPriceTest, FailOnParcelTooLarge) {
+TEST(ReindeerPricesTest, UnsupportedParcels) {
     double const delta = 0.01;
     EXPECT_THROW(get_best_price({120 + delta, 60, 60, 30}, reindeer_prices), std::runtime_error);
     EXPECT_THROW(get_best_price({120, 60 + delta, 60, 30}, reindeer_prices), std::runtime_error);
     EXPECT_THROW(get_best_price({120, 60, 60, 30 + delta}, reindeer_prices), std::runtime_error);
 }
 
-TEST(GetReindeerPriceTest, LowestPrice) {
-    EXPECT_EQ(get_best_price({1, 1, 1, 1}, reindeer_prices), 2.99);
-    EXPECT_EQ(get_best_price({30, 20, 20, 2}, reindeer_prices), 2.99);
-}
-
-TEST(GetReindeerPriceTest, SecondLowestPriceLowerBound) {
+TEST(ReindeerPricesTest, SupportedParcels) {
     double const delta = 0.01;
-    EXPECT_EQ(get_best_price({30 + delta, 20, 20, 2}, reindeer_prices), 4.99);
-    EXPECT_EQ(get_best_price({30, 20 + delta, 20, 2}, reindeer_prices), 4.99);
-    EXPECT_EQ(get_best_price({30, 20, 20 + delta, 2}, reindeer_prices), 4.99);
-    EXPECT_EQ(get_best_price({30, 20, 20, 2 + delta}, reindeer_prices), 4.99);
-}
+    std::vector<std::pair<parcel, double>> expectations = {
+        {{1, 1, 1, 1}, 2.99},
+        {{30, 20, 20, 2}, 2.99},
 
-TEST(GetReindeerPriceTest, SecondLowestPriceUpperBound) {
-    EXPECT_EQ(get_best_price({40, 30, 20, 5}, reindeer_prices), 4.99);
+        {{30 + delta, 20, 20, 2}, 4.99},
+        {{30, 20 + delta, 20, 2}, 4.99},
+        {{30, 20, 20 + delta, 2}, 4.99},
+        {{30, 20, 20, 2 + delta}, 4.99},
+        {{40, 30, 20, 5}, 4.99},
+
+        {{40 + delta, 10, 20, 4}, 5.99},
+        {{40, 30 + delta, 10, 4}, 5.99},
+        {{60, 10, 60, 4}, 5.99},
+
+        {{60 + delta, 10, 60, 4}, 14.99},
+        {{60, 10 + delta, 60, 4}, 14.99},
+        {{60, 10, 60, 4 + delta}, 14.99},
+        {{40 + delta, 30, 20, 5}, 14.99},
+        {{40, 30 + delta, 20, 5}, 14.99},
+        {{40, 30, 20 + delta, 5}, 14.99},
+        {{40, 30, 20, 5 + delta}, 14.99},
+        {{120, 60, 60, 30}, 14.99}
+    };
+
+    for (auto const & expectation : expectations) {
+        auto const & parcel = expectation.first;
+        auto const expected_price = expectation.second;
+        EXPECT_EQ(get_best_price(parcel, reindeer_prices), expected_price);
+    }
 }
 
 TEST(GetReindeerPriceTest, SecondLowestPriceWithSwappedDimensions) {
     double const delta = 0.01;
     EXPECT_EQ(get_best_price({20, 30 + delta, 20, 2}, reindeer_prices), 4.99);
     EXPECT_EQ(get_best_price({20, 20, 30 + delta, 2}, reindeer_prices), 4.99);
-}
-
-TEST(GetReindeerPriceTest, ThirdLowestPriceLowerBound) {
-    double const delta = 0.01;
-    EXPECT_EQ(get_best_price({40 + delta, 10, 20, 4}, reindeer_prices), 5.99);
-    EXPECT_EQ(get_best_price({40, 30 + delta, 10, 4}, reindeer_prices), 5.99);
-}
-
-TEST(GetReindeerPriceTest, ThirdLowestPriceUpperBound) {
-    EXPECT_EQ(get_best_price({60, 10, 60, 4}, reindeer_prices), 5.99);
-}
-
-TEST(GetReindeerPriceTest, LargestPriceLowerBound) {
-    double const delta = 0.01;
-    EXPECT_EQ(get_best_price({60 + delta, 10, 60, 4}, reindeer_prices), 14.99);
-    EXPECT_EQ(get_best_price({60, 10 + delta, 60, 4}, reindeer_prices), 14.99);
-    EXPECT_EQ(get_best_price({60, 10, 60, 4 + delta}, reindeer_prices), 14.99);
-    EXPECT_EQ(get_best_price({40 + delta, 30, 20, 5}, reindeer_prices), 14.99);
-    EXPECT_EQ(get_best_price({40, 30 + delta, 20, 5}, reindeer_prices), 14.99);
-    EXPECT_EQ(get_best_price({40, 30, 20 + delta, 5}, reindeer_prices), 14.99);
-    EXPECT_EQ(get_best_price({40, 30, 20, 5 + delta}, reindeer_prices), 14.99);
-}
-
-TEST(GetReindeerPriceTest, LargestPriceUpperBound) {
-    EXPECT_EQ(get_best_price({120, 60, 60, 30}, reindeer_prices), 14.99);
 }
